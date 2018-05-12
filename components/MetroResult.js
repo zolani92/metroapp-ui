8d0234ac49403f90198e8da16af65f27cb33f545
rlam <rlam@egencia.com>
2018-05-12 18:26:56 +0200

    Result page

--- components/MetroResult.js
+++ components/MetroResult.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { StyleSheet, ScrollView, Text, View } from "react-native"
-import { Card, Icon } from "react-native-elements"
+import { Card, Icon, ListItem } from "react-native-elements"
 import { connect } from 'react-redux'
 import actions from '../actions'
 import moment from 'moment'
@@ -12,11 +12,11 @@ class MetroResult extends Component {
 
     render() {
         const { shortestPath } = this.props
-        const { departureStationName, 
-                arrivalStationName,
-                stationsCount,
-                transfersCount,
-                itinerary } = shortestPath
+        const { departureStationName,
+            arrivalStationName,
+            stationsCount,
+            transfersCount,
+            itinerary } = shortestPath
 
         /* 
             const linesList = itinerary.map((itineraryPart) => {
@@ -35,9 +35,39 @@ class MetroResult extends Component {
             });
         */
 
+        const stationsCountStr = (stationsCount > 1) ? `${stationsCount} stops` : `${stationsCount} stop`
+        const transfersCountStr = (transfersCount > 1) ? `${transfersCount} connections` : `${transfersCount} connection`
+
         return (
             <ScrollView style={styles.container}>
-                <Text>Results page</Text>
+                <Card>
+                    <View style={styles.infoCard}>
+                        <Text style={{ flex: 1, textAlign: 'left' }}>{stationsCountStr}</Text>
+                        <Text style={{ flex: 1, textAlign: 'right' }}>{transfersCountStr}</Text>
+                    </View>
+                </Card>
+                <Card>
+                    <View style={styles.endPointsCard}>
+                        <Text style={{ flex: 1, textAlign: 'center' }}>{departureStationName}</Text>
+                    </View>
+                </Card>
+                {itinerary.map((itineraryPart) => {
+                    <Card title={itineraryPart.line}>
+                        <View style={styles.endPointsCard}>
+                            {itineraryPart.stationsNames.map((stationName, i) => {
+                                <ListItem
+                                    key={i}
+                                    title={stationName}
+                                />
+                            })}
+                        </View>
+                    </Card>
+                })}
+                <Card>
+                    <View style={styles.endPointsCard}>
+                        <Text style={{ flex: 1, textAlign: 'center' }}>{arrivalStationName}</Text>
+                    </View>
+                </Card>
             </ScrollView>
         )
     }
@@ -48,9 +78,15 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F0F1F3'
     },
-    mainCard: {
+    infoCard: {
         flex: 1,
         flexDirection: 'row'
+    },
+    endPointsCard: {
+        flex: 1,
+    },
+    linesCard: {
+        flex: 1,
     }
 })
 
